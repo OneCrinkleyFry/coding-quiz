7e9705e92cb3bc7dcfcd97cacb9cf099c9138280
JoshMiller5994 <talk2joshbgosh@gmail.com>
2020-08-22 17:09:25 -0600

    randomized the questions.

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,67 +5,84 @@ var questions = [
     {
         q: "0What is JavaScript?",
         correct: "an object oriented coding language for web development.",
-        incorrect1: "a potato",
-        incorrect2: "a potato",
-        incorrect3: "a potato",
+        incorrect1: "1",
+        incorrect2: "2",
+        incorrect3: "3"
     },
     {
         q: "1What is JavaScript?",
         correct: "an object oriented coding language for web development.",
-        incorrect1: "a potato",
-        incorrect2: "a potato",
-        incorrect3: "a potato",
+        incorrect1: "1",
+        incorrect2: "2",
+        incorrect3: "3"
     },
     {
         q: "2What is JavaScript?",
         correct: "an object oriented coding language for web development.",
-        incorrect1: "a potato",
-        incorrect2: "a potato",
-        incorrect3: "a potato",
+        incorrect1: "1",
+        incorrect2: "2",
+        incorrect3: "3"
     },
     {
         q: "3What is JavaScript?",
         correct: "an object oriented coding language for web development.",
-        incorrect1: "a potato",
-        incorrect2: "a potato",
-        incorrect3: "a potato",
+        incorrect1: "1",
+        incorrect2: "2",
+        incorrect3: "3"
     },
     {
         q: "4What is JavaScript?",
         correct: "an object oriented coding language for web development.",
-        incorrect1: "a potato",
-        incorrect2: "a potato",
-        incorrect3: "a potato",
+        incorrect1: "1",
+        incorrect2: "2",
+        incorrect3: "3"
     }
 ];
 var questionsLeft = questions.length;
 
 var createAnswers = function(answersBody, question) {
+    var orderedAnswers = [question.correct, question.incorrect1, question.incorrect2, question.incorrect3];
+    var orderedInitialLength = orderedAnswers.length;
+    var unorderedAnswers = [];
+
+    while (unorderedAnswers.length < orderedInitialLength) {
+        console.log(orderedAnswers);
+        console.log(unorderedAnswers);
+        var index = Math.floor(Math.random() * orderedAnswers.length);
+        unorderedAnswers.push(orderedAnswers[index]);
+        orderedAnswers.splice(index,1);
+        console.log(orderedAnswers);
+        console.log(unorderedAnswers);
+    }
+    
+    for (let i = 0; i < unorderedAnswers.length; i++) {
+        var 
+        
+    }
 }
 
 var createQuestions = function(question) {
 
         var questionTitleEl = document.createElement("h1");
+        
         questionTitleEl.className ="question-title";
-
         questionTitleEl.textContent = question.q;
-        console.dir(questionTitleEl);
         quizBodyEl.appendChild(questionTitleEl);
 
         var answersEl = document.createElement("div");
+
         answersEl.className = "question-answers";
         quizBodyEl.appendChild(answersEl);
 
         createAnswers(answersEl, question);
-
 }
 
 var runGame = function() {
     startBtnEl.remove();
-    for (var i = 0; timeLeft > 0 && i < questions.length; i++) {
-        console.log(i);
-        createQuestions(questions[i]);
-    }
+    //for (var i = 0; timeLeft > 0 && i < questions.length; i++) {
+        //createQuestions(questions[i]);
+    //}
+    createQuestions(questions[0]);
 };
 
 startBtnEl.addEventListener("click", runGame);
