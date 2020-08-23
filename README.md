540f6724e87570f314b7840a00e1b15ae1a2fd5b
JoshMiller5994 <talk2joshbgosh@gmail.com>
2020-08-22 20:23:48 -0600

    added real questions instead of place holders.

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # coding-quiz
+
+
+## Purpose
+To assess how well I am understanding javascript principles.
+
+## sources
+
+[the questions can be found here](http://mcqspdfs.blogspot.com/2013/08/60-top-javascript-multiple-choice.html)
+
+## Made by:
+# Josh Miller
+
+### [Where can you find the project](sithSlave.github.io/coding-quiz/)
+
+
--- assets/js/script.js
+++ assets/js/script.js
@@ -8,39 +8,171 @@ var iterator = 0;
 var wasAnswered = true;
 var questions = [
     {
-        q: "0What is JavaScript?",
-        correct: "an object oriented coding language for web development.",
-        incorrect1: "1",
-        incorrect2: "2",
-        incorrect3: "3"
+        q: "Why so JavaScript and Java have similar name?",
+        correct: "JavaScript's syntax is loosely based on Java's",
+        incorrect1: "JavaScript is a stripped-down version of Java",
+        incorrect2: "They both originated on the island of Java",
+        incorrect3: "None of the above"
     },
     {
-        q: "1What is JavaScript?",
-        correct: "an object oriented coding language for web development.",
-        incorrect1: "1",
-        incorrect2: "2",
-        incorrect3: "3"
+        q: "When a user views a page containing a JavaScript program, which machine actually executes the script?",
+        correct: "The User's machine running a Web browser",
+        incorrect1: "The Web server",
+        incorrect2: "A central machine deep within Netscape's corporate offices",
+        incorrect3: "None of the above"
     },
     {
-        q: "2What is JavaScript?",
-        correct: "an object oriented coding language for web development.",
-        incorrect1: "1",
-        incorrect2: "2",
-        incorrect3: "3"
+        q: "______ JavaScript is also called client-side JavaScript.",
+        correct: "Navigator",
+        incorrect1: "Microsoft",
+        incorrect2: "LiveWire",
+        incorrect3: "Native"
     },
     {
-        q: "3What is JavaScript?",
-        correct: "an object oriented coding language for web development.",
-        incorrect1: "1",
-        incorrect2: "2",
-        incorrect3: "3"
+        q: "__________ JavaScript is also called server-side JavaScript.",
+        correct: "LiveWire",
+        incorrect1: "Microsoft",
+        incorrect2: "Navigator",
+        incorrect3: "Native"
     },
     {
-        q: "4What is JavaScript?",
-        correct: "an object oriented coding language for web development.",
-        incorrect1: "1",
-        incorrect2: "2",
-        incorrect3: "3"
+        q: "What are variables used for in JavaScript Programs?",
+        correct: "Storing numbers, dates, or other values",
+        incorrect1: "Varying randomly",
+        incorrect2: "Causing high-school algebra flashbacks",
+        incorrect3: "None of the above"
+    },
+    {
+        q: "_____ JavaScript statements embedded in an HTML page can respond to user events such as mouse-clicks, form input, and page navigation.",
+        correct: "Client-side",
+        incorrect1: "Server-side",
+        incorrect2: "Local",
+        incorrect3: "Native"
+    },
+    {
+        q: "What should appear at the very end of your JavaScript? The <script LANGUAGE='JavaScript'>tag",
+        correct: "The </script>",
+        incorrect1: "The <script>",
+        incorrect2: "The END statement",
+        incorrect3: "None of the above"
+    },
+    {
+        q: "Which of the following can't be done with client-side JavaScript?",
+        correct: "Storing the form's contents to a database file on the server",
+        incorrect1: "Validating a form",
+        incorrect2: "Sending a form's contents by email",
+        incorrect3: "None of the above"
+    },
+    {
+        q: "Which of the following are capabilities of functions in JavaScript?",
+        correct: "Accept parameters",
+        incorrect1: "Return a value",
+        incorrect2: "Accept parameters and Return a value",
+        incorrect3: "None of the above"
+    },
+    {
+        q: "Which of the following is not a valid JavaScript variable name?",
+        correct: "2names",
+        incorrect1: "_first_and_last_names",
+        incorrect2: "FirstAndLast",
+        incorrect3: "None of the above"
+    },
+    {
+        q: "______ tag is an extension to HTML that can enclose any number of JavaScript statements.",
+        correct: "<SCRIPT>",
+        incorrect1: "<BODY>",
+        incorrect2: "<HEAD>",
+        incorrect3: "<TITLE>"
+    },
+    {
+        q: "How does JavaScript store dates in a date object?",
+        correct: "The number of milliseconds since January 1st, 1970",
+        incorrect1: "The number of days since January 1st, 1900",
+        incorrect2: "The number of seconds since Netscape's public stock offering.",
+        incorrect3: "None of the above"
+    },
+    {
+        q: "Which of the following attribute can hold the JavaScript version?",
+        correct: "LANGUAGE",
+        incorrect1: "SCRIPT",
+        incorrect2: "VERSION",
+        incorrect3: "None of the above"
+    },
+    {
+        q: "What is the correct JavaScript syntax to write 'Hello World'?",
+        correct: "document.write('Hello World')",
+        incorrect1: "System.out.println('Hello World')",
+        incorrect2: "println ('Hello World')",
+        incorrect3: "response.write('Hello World')"
+    },
+    {
+        q: "Inside which HTML element do we put the JavaScript?",
+        correct: "<script>",
+        incorrect1: "<js>",
+        incorrect2: "<scripting>",
+        incorrect3: "<javascript>"
+    },
+    {
+        q: "Which types of image maps can be used with JavaScript?",
+        correct: "Client-side image maps",
+        incorrect1: "Server-side image maps",
+        incorrect2: "Server-side image maps and Client-side image maps",
+        incorrect3: "None of the above"
+    },
+    {
+        q: "Which of the following navigator object properties is the same in both   Netscape and IE?",
+        correct: "navigator.appCodeName",
+        incorrect1: "navigator.appName",
+        incorrect2: "navigator.appVersion",
+        incorrect3: "None of the above"
+    },
+    {
+        q: "What does the <noscript> tag do?",
+        correct: "Enclose text to be displayed by non-JavaScript browsers.",
+        incorrect1: "Prevents scripts on the page from executing.",
+        incorrect2: "Describes certain low-budget movies.",
+        incorrect3: "None of the above"
+    },
+    {
+        q: "JavaScript entities start with _______ and end with _________.",
+        correct: "Ampersand, semicolon",
+        incorrect1: "Semicolon, colon",
+        incorrect2: "Semicolon, Ampersand",
+        incorrect3: "Ampersand, colon"
+    },
+    {
+        q: "Which of the following best describes JavaScript?",
+        correct: "an object-oriented scripting language.",
+        incorrect1: "a low-level programming language.",
+        incorrect2: "a scripting language precompiled in the browser.",
+        incorrect3: "a compiled scripting language."
+    },
+    {
+        q: "Choose the server-side JavaScript object?",
+        correct: "File",
+        incorrect1: "FileUpLoad",
+        incorrect2: "Function",
+        incorrect3: "Date"
+    },
+    {
+        q: "Choose the client-side JavaScript object?",
+        correct: "FileUpLoad",
+        incorrect1: "Database",
+        incorrect2: "Cursor",
+        incorrect3: "Client"
+    },
+    {
+        q: "Which of the following is not considered a JavaScript operator?",
+        correct: "this",
+        incorrect1: "new",
+        incorrect2: "delete",
+        incorrect3: "typeof"
+    },{
+        q: "______method evaluates a string of JavaScript code in the context of the specified",
+        correct: "Eval",
+        incorrect1: "ParseInt",
+        incorrect2: "ParseFloat",
+        incorrect3: "Efloat"
     }
 ];
 var questionsLeft = questions.length;
@@ -117,9 +249,6 @@ var checkCorrect = function(event) {
     if (event.target.matches(".answer-btn")) {
        if (event.target.matches("#true.answer-btn")) {
             answersCorrect++;
-            alert("you got this correct");
-        } else {
-            alert("you got this incorrect");
         }
         removeQuestions();
         wasAnswered = true;
